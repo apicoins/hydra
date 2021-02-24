073253e40b4dcfc17f3f12d6a7cbd22466d51df7
apicoins <79605089+apicoins@users.noreply.github.com>
2021-02-24 23:24:04 +0300

    Add files via upload

---
+++ precache-manifest.ce353ba02a0fa31c786f965bd0f3edae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716ba361bd24b15d09930611324bb3e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa3e97b18da06a55413a",
+    "url": "./static/css/2.fb75dcb6.chunk.css"
+  },
+  {
+    "revision": "310f4b4f092ce02281be",
+    "url": "./static/css/main.cdd64874.chunk.css"
+  },
+  {
+    "revision": "fa3e97b18da06a55413a",
+    "url": "./static/js/2.5e88e008.chunk.js"
+  },
+  {
+    "revision": "59be1b5a4c2910c660bfb9986f585334",
+    "url": "./static/js/2.5e88e008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "310f4b4f092ce02281be",
+    "url": "./static/js/main.b133820e.chunk.js"
+  },
+  {
+    "revision": "8713c500a83b98e9d55b",
+    "url": "./static/js/runtime-main.0e900956.js"
+  },
+  {
+    "revision": "1808b5f6b2dd330757f13c6dc3ddf32a",
+    "url": "./static/media/1233.1808b5f6.ttf"
+  },
+  {
+    "revision": "ff835e5106e97b0d98fa21a820cb2340",
+    "url": "./static/media/click.ff835e51.png"
+  },
+  {
+    "revision": "3b7e73d7332b5214263ae3d8a2f00393",
+    "url": "./static/media/coin.3b7e73d7.png"
+  }
+]);
